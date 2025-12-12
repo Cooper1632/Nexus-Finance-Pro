a94fe17ba51464f997e741e9cdd8f33accac4fcc
Cooper1632 <logiqc@gmail.com>
2025-12-12 10:51:26 -0500

    Version 1.04

--- server/probe_yahoo.js
+++
@@ -1,6 +0,0 @@
-import * as yf from 'yahoo-finance2';
-console.log('Keys:', Object.keys(yf));
-console.log('Default:', yf.default);
-try {
-    console.log('Default prototype:', Object.getPrototypeOf(yf.default));
-} catch (e) { }
